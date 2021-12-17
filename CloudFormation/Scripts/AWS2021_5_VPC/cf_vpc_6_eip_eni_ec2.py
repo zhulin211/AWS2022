0a1f44b7f86b3472242ed49608c5b84dc4293aed
administrator <collinsctk@qytang.com>
2021-12-17 21:00:04 +0800

    VPC搞定

---
+++ CloudFormation/Scripts/AWS2021_5_VPC/cf_vpc_6_eip_eni_ec2.py
@@ -0,0 +1,15 @@
+from Scripts.ctf_0_basic_functions import create_update_cf
+
+import boto3
+from CloudFormation.region import region
+client = boto3.client('cloudformation', region_name=region)
+
+if __name__ == "__main__":
+    template_path = '../../AWS2021_5_VPC/vpc_6_eip_eni_ec2.yaml'
+    stack_name = 'VPC6EIPENI'
+    parameters = [
+        {'ParameterKey': "VPCStackName", "ParameterValue": 'VPC1VPC'},
+        {'ParameterKey': "SubnetStackName", "ParameterValue": 'VPC2Subnets'},
+        {'ParameterKey': "SecurityGroupStackName", "ParameterValue": 'VPC5SecurityGroup'}
+    ]
+    print(create_update_cf(stack_name, template_path, parameters=parameters))
---
+++ CloudFormation/Scripts/AWS2021_5_VPC/cf_vpc_7_nat_gateway.py
@@ -0,0 +1,14 @@
+from Scripts.ctf_0_basic_functions import create_update_cf
+
+import boto3
+from CloudFormation.region import region
+client = boto3.client('cloudformation', region_name=region)
+
+if __name__ == "__main__":
+    template_path = '../../AWS2021_5_VPC/vpc_7_natgw.yaml'
+    stack_name = 'VPC7NATGW'
+    parameters = [
+        {'ParameterKey': "VPCStackName", "ParameterValue": 'VPC1VPC'},
+        {'ParameterKey': "SubnetStackName", "ParameterValue": 'VPC2Subnets'},
+    ]
+    print(create_update_cf(stack_name, template_path, parameters=parameters))

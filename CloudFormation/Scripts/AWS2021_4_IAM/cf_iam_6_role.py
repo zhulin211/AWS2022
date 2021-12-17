7388640510ffc7fdd30268769d44eea57a434b75
administrator <collinsctk@qytang.com>
2021-12-17 17:08:34 +0800

    IAM搞定

---
+++ CloudFormation/Scripts/AWS2021_4_IAM/cf_iam_6_role.py
@@ -0,0 +1,10 @@
+from Scripts.ctf_0_basic_functions import create_update_cf
+
+import boto3
+from CloudFormation.region import region
+client = boto3.client('cloudformation', region_name=region)
+
+if __name__ == "__main__":
+    template_path = '../../AWS2021_4_IAM/iam_6_role.yaml'
+    stack_name = 'IAMRole'
+    print(create_update_cf(stack_name, template_path))

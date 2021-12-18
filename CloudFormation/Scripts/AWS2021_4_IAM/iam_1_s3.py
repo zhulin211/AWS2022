b951abbac94b9e3ac8d0524d76e6f0963caae99c
administrator <collinsctk@qytang.com>
2021-12-18 19:37:48 +0800

    不死就改名2022

--- CloudFormation/Scripts/AWS2021_4_IAM/iam_1_s3.py
+++
@@ -1,8 +0,0 @@
-from CloudFormation.Scripts.AWS2021_3_S3.s3_1_upload import s3_upload
-from CloudFormation.Scripts.AWS2021_3_S3.s3_2_tagging import s3_tagging
-
-
-if __name__ == '__main__':
-    s3_upload('../AWS2021_3_S3/files/index.html', 'aws2021-iam-s3', 'index.html')
-    s3_upload('../AWS2021_3_S3/files/error.html', 'aws2021-iam-s3', 'error.html')
-    s3_tagging(['index.html', 'error.html'], 'aws2021-iam-s3', [{'Key': 'web_file', 'Value': 'http_file'}])

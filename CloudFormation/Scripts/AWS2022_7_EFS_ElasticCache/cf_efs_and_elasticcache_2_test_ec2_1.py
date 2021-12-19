801d2c3ddb0791330ad7dbad422eaa048452af9f
administrator <collinsctk@qytang.com>
2021-12-19 22:36:20 +0800

    redis 搞定

--- CloudFormation/Scripts/AWS2022_7_EFS_ElasticCache/cf_efs_and_elasticcache_2_test_ec2_1.py
+++ CloudFormation/Scripts/AWS2022_7_EFS_ElasticCache/cf_efs_and_elasticcache_2_test_ec2_1.py
@@ -6,7 +6,7 @@ client = boto3.client('cloudformation', region_name=region)
 
 if __name__ == "__main__":
     template_path = '../../AWS2022_7_EFS_ElasticCache/efs_and_elasticcache_2_test_ec2_1.yaml'
-    stack_name = 'EFSRadisEC21'
+    stack_name = 'EFSRedisEC21'
     parameters = [
         {'ParameterKey': "VPCStackName", "ParameterValue": 'VPC1VPC'},
         {'ParameterKey': "SubnetStackName", "ParameterValue": 'VPC2Subnets'},

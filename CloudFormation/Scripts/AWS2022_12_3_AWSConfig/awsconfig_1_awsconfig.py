3ecc05bda01acfd2f2c66a4fbc53d396f655c04c
administrator <collinsctk@qytang.com>
2021-12-23 16:06:16 +0800

    aws config搞定

--- CloudFormation/Scripts/AWS2022_12_3_AWSConfig/awsconfig_1_awsconfig.py
+++ CloudFormation/Scripts/AWS2022_12_3_AWSConfig/awsconfig_1_awsconfig.py
@@ -4,4 +4,5 @@ from Scripts.ctf_0_basic_functions import create_update_cf
 if __name__ == "__main__":
     template_path = '../../AWS2022_12_3_AWSConfig/awsconfig_1_awsconfig.yaml'
     stack_name = 'AWSConfig'
+    # 注意是us-weast-1
     print(create_update_cf(stack_name, template_path, region='us-west-1'))
